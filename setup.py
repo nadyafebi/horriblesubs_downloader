6ae581295014045f04eb7aac6b56f8c7b7857765
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2018-07-17 20:50:27 -0700

    Initialize setup.py and package

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+# Metadata
+NAME = 'horriblesubs_downloader'
+VERSION = ''
+DESCRIPTION = ''
+AUTHOR = 'Nadya Djojosantoso'
+EMAIL = 'hello@nadyafebi.com'
+URL = 'https://github.com/nadyafebi/horriblesubs_downloader'
+
+# Packages
+PACKAGES = find_packages(exclude=['*.tests'])
+REQUIRED = []
+
+# CLI Settings
+COMMAND = 'hsd'
+ENTRY = {
+    'console_scripts': ['{}={}.__main__:main'.format(COMMAND, NAME)]
+}
+
+# Magic begins here
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=PACKAGES,
+    install_requires=REQUIRED,
+    entry_points=ENTRY
+)
